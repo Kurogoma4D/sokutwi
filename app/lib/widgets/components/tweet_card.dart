45d4a381b8e5cf86b4d84151b48e7f550ed7e9c8
Kurogoma4D <contact@krgm4d.dev>
2023-01-01 13:59:52 +0900

    modify: design

--- app/lib/widgets/components/tweet_card.dart
+++ app/lib/widgets/components/tweet_card.dart
@@ -9,8 +9,8 @@ class TweetCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Card(
-      color: Colors.amber.shade50,
+    return CustomPaint(
+      painter: _CardPainter(),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
         child: Center(
@@ -25,4 +25,58 @@ class TweetCard extends ConsumerWidget {
       ),
     );
   }
+}
+
+class _CardPainter extends CustomPainter {
+  final cutRadius = 40.0;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final p = Paint()
+      ..color = Colors.amber.shade50
+      ..style = PaintingStyle.fill;
+    final stroke = Paint()
+      ..color = Colors.black87
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.0;
+
+    final path = Path()
+      ..moveTo(4.0, 0.0)
+      ..lineTo(size.width - 4, 0.0)
+      ..relativeArcToPoint(
+        const Offset(4, 4),
+        radius: const Radius.circular(4),
+      )
+      ..lineTo(size.width, size.height - cutRadius * 0.8)
+      ..relativeLineTo(-cutRadius * 1.6, cutRadius * 0.8)
+      ..lineTo(4.0, size.height)
+      ..relativeArcToPoint(
+        const Offset(-4, -4),
+        radius: const Radius.circular(4),
+      )
+      ..close();
+    final strokePath = Path()
+      ..moveTo(4.0, 0.0)
+      ..lineTo(size.width - 4, 0.0)
+      ..relativeArcToPoint(
+        const Offset(4, 4),
+        radius: const Radius.circular(4),
+      )
+      ..lineTo(size.width, size.height - cutRadius)
+      ..relativeQuadraticBezierTo(0, cutRadius / 2, -cutRadius * 0.6, 0)
+      ..quadraticBezierTo(size.width - cutRadius, size.height,
+          size.width - cutRadius * 2, size.height)
+      ..lineTo(4.0, size.height)
+      ..relativeArcToPoint(
+        const Offset(-4, -4),
+        radius: const Radius.circular(4),
+      )
+      ..close();
+
+    canvas.drawPath(path, p);
+    canvas.drawPath(strokePath, stroke);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
